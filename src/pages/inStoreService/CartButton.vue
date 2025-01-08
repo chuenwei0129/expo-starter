c10a63c15ebda4a23a63a19edb48ac29b5944a65
chuenwei0129 <chuenwei0129@gmail.com>
2025-01-08 20:52:33 +0800

    feat(inStoreService): 添加定位权限管理、资源列表及相关组件和API模拟数据

---
+++ src/pages/inStoreService/CartButton.vue
@@ -0,0 +1,98 @@
+<template>
+  <view
+    class="cart-button"
+    @click="handleClick"
+  >
+    <view
+      v-if="cartItemCount"
+      class="cart-button__badge"
+    >
+      <u-icon
+        v-if="cartItemCount > 99"
+        name="more-dot-fill"
+        color="#fff"
+        size="12"
+      />
+      <text v-else>
+        {{ cartItemCount }}
+      </text>
+    </view>
+
+    <i
+      class="iconfont icon-a-ShoppingCart"
+      style="font-size: 46rpx"
+    />
+  </view>
+</template>
+
+<script>
+import { GET_CART_TOTAL } from '@/api/mallv2'
+import { action_report } from '@/utils/track'
+
+export default {
+  name: 'CartButton',
+  data () {
+    return {
+      cartItemCount: 0,
+    }
+  },
+  mounted () {
+    this.getCartNum()
+  },
+
+  methods: {
+    handleClick () {
+      const userId =
+        this.$dsBridge.call('getUserId', 'getUserId') || '401211901598814208'
+      action_report({
+        action_name: 'service_shoppingcart_click',
+        module_name: 'service',
+        extend: {
+            user_id: userId,
+          },
+      })
+
+      this.$dsBridge.call('gotoPageThroughRoute', {
+        page: `${window.location.origin}/crm-medical-h5/#/pagesC/shoppingCart/index`,
+      })
+    },
+    //获取购物车数量
+    async getCartNum () {
+      const res = await GET_CART_TOTAL({
+        checked: false,
+        cartType: 1,
+      })
+      const { data } = res.data
+      this.cartItemCount = data
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.cart-button {
+  position: relative;
+  width: 70rpx; // 或与图标大小匹配的值
+  height: 70rpx;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .cart-button__badge {
+    position: absolute;
+    top: -8rpx; // 调整为适当值
+    right: -8rpx;
+    background: #f33a50;
+    font-size: 20rpx;
+    font-weight: 600;
+    color: #ffffff;
+    border-radius: 50%;
+    width: 34rpx;
+    height: 34rpx;
+    /* 使用 flex 布局使内容居中 */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/pages/inStoreService/SearchBar.vue
@@ -0,0 +1,161 @@
+<template>
+  <view
+    class="search-bar"
+    @click="$u.debounce(goToSearchPage, 500)"
+  >
+    <view class="search-bar__input">
+      <view class="search-bar__placeholder">
+        <scan-code
+          color="red"
+        />
+        <view class="search-bar__divider" />
+        <transition name="slide">
+          <text
+            :key="currentKeyword"
+            class="search-bar__text"
+          >
+            {{ currentKeyword }}
+          </text>
+        </transition>
+      </view>
+      <view class="search-bar__button">
+        <image
+          class="search-bar__icon"
+          src="https://frontend-cdn.chongpangpang.com/image/medical-mp/index2/header-search.png"
+          alt="Search Icon"
+        />
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+import ScanCode from '@/components/scanCode/index.vue'
+// import { fetchHotWordAPI } from './api/mockAPI'
+import {fetchHotWordAPI} from './api/inStoreService'
+import { action_report } from '@/utils/track'
+
+export default {
+  name: 'SearchBar',
+  components: { ScanCode },
+  data () {
+    return {
+      hotKeywords: [], // 热词列表
+      currentKeyword: '搜你想要的到店服务名称', // 当前显示的热词
+      loopTimer: null, // 热词轮播定时器
+      rollTime: 5000, // 轮播时间间隔
+    }
+  },
+  mounted () {
+    this.fetchHotWordData()
+  },
+  beforeDestroy () {
+    clearInterval(this.loopTimer)
+    this.loopTimer = null
+  },
+  methods: {
+    goToSearchPage () {
+      const userId =
+        this.$dsBridge.call('getUserId', 'getUserId') || '401211901598814208'
+      action_report({
+        action_name: 'service_search_click',
+        module_name: 'service',
+        extend: {
+            user_id: userId,
+          },
+      })
+
+      this.$dsBridge.call('gotoPageThroughRoute', {
+        page: '/home/search?scene=5&keyword=' + this.currentKeyword,
+      })
+    },
+    // 获取热词列表
+    async fetchHotWordData () {
+      const resp = await fetchHotWordAPI()
+      this.hotKeywords = resp.data.hotWords || []
+      this.rollTime = resp.data.rollTime * 1000
+      this.startKeywordRotation() // 获取到热词后开始轮播
+    },
+
+    // 开始热词轮播
+    startKeywordRotation () {
+      if (this.hotKeywords.length > 0) {
+        let currentIndex = 0
+        this.currentKeyword = this.hotKeywords[currentIndex] // 初始化显示第一个热词
+        this.loopTimer = setInterval(() => {
+          currentIndex = (currentIndex + 1) % this.hotKeywords.length
+          this.currentKeyword = this.hotKeywords[currentIndex]
+        }, this.rollTime) // 每 5 秒切换一次
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@import '@/utils/fn.scss';
+.search-bar {
+  width: 100%;
+  border-radius: 18px;
+  border: 3rpx solid #fe2442;
+
+  &__input {
+    width: 100%;
+    height: 63rpx;
+    border-radius: 35rpx;
+    background: white;
+    display: flex;
+    align-items: center;
+    padding: 3rpx;
+  }
+
+  &__placeholder {
+    color: #1f1f1f;
+    font-size: 27rpx;
+    margin-left: 20rpx;
+    display: flex;
+    align-items: center;
+    font-weight: 300;
+  }
+
+  &__divider {
+    width: 2rpx;
+    height: 35rpx;
+    background-color: #dddddd;
+    margin: 0 20rpx;
+  }
+
+  &__text {
+    @include ellipsis(300rpx);
+    font-size: inherit;
+    color: inherit;
+  }
+
+  &__button {
+    background-color: #fe2442;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 88rpx;
+    height: 54rpx;
+    border-radius: 27rpx;
+    margin-left: auto;
+    margin-right: 5rpx;
+  }
+
+  &__icon {
+    width: 20px;
+    height: 20px;
+  }
+}
+
+// 过渡动画
+.slide-enter-active, .slide-leave-active {
+  transition: all 0.5s ease;
+}
+.slide-enter, .slide-leave-to {
+  transform: translateY(100%);
+  opacity: 0;
+}
+</style>
